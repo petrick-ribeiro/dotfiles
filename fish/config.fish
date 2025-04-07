1432078ff7eb40554f2a0df2ce26278006187f3e
Petrick Ribeiro <ptrck1110@protonmail.com>
2025-04-07 17:12:40 -0300

    feat: add completions and scripts

--- fish/config.fish
+++ fish/config.fish
@@ -2,16 +2,24 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   set fish_greeting
   neofetch 
-  starship init fish | source
+  starship init fish --print-full-init | sed 's/"$(commandline)"/(commandline | string collect)/' | source 
 
+  #######################################
+	# $PATH
+  #######################################
   # Kitty
   # set -gx PATH $PATH $HOME/.local/kitty.app/bin/
+  # alias kdev='kitty --session dev_session.conf'
 
   # Walk
   set -gx WALK_EDITOR /usr/local/bin/nvim
 
-  #Rust
+  # Rust
   set -gx PATH $PATH $HOME/.cargo/bin/
+
+  # JS
+  set -gx NVM_DIR $HOME/.nvm
+  set -gx PNPM_HOME "/home/petrick/.local/share/pnpm"
 
   # GO
   set -gx PATH $PATH /usr/local/go/bin/
@@ -28,16 +36,25 @@ if status is-interactive
   # K8s
   # set -gx PATH $PATH $HOME/.krew/bin
 
+  #######################################
+	# ALIASES
+  #######################################
   # Aliases
-  # alias k=kubectl
-  # alias kd=kind
   alias lg=lazygit
-  # alias mk=minikube
   alias v=vagrant
   alias tb='nc termbin.com 9999'
   alias tf=terraform
+  alias pn=pnpm
+  alias upgrade='sudo apt-get update && sudo apt-get upgrade'
+  # alias k=kubectl
+  # alias kd=kind
+  # alias mk=minikube
 end
 
+
+#######################################
+# FUNCTIONS
+#######################################
 # Kitty Shell Integration
 if set -q KITTY_INSTALLATION_DIR
     set --global KITTY_SHELL_INTEGRATION enabled
@@ -48,4 +65,31 @@ end
 # Walk function
 function lk
   set loc (walk --icons $argv); and cd $loc;
+end
+
+# PNPM $PATH function
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+
+# Kitty Sessions
+function kdev
+    # Verify shell args
+    if test (count $argv) -ne 1
+        echo "USAGE: kdev <project_path>"
+        return 1
+    end
+
+    # Check if kitty is installed
+    if not type -q kitty
+      echo "Kitty are not on PATH."
+      return 1
+    end
+
+    # Set project path and execute Kitty
+    set -x PROJECT_PATH $argv[1]
+    # nohup kitty --session $HOME/.dotfiles/kitty/dev_session.conf & disown
+    kitty --detach --session $HOME/.dotfiles/kitty/dev_session.conf
+    kitten notify --icon kitty "[DEV]" Extreme Go Horse mode activated!
+    exit
 end
